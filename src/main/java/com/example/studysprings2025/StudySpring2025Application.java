abf6568f29e60be9bba4b153ac69aefcf192d452
Dmytro Krupa <dima-krupa@ukr.net>
2025-11-04 21:30:47 +0200

    Initial commit

---
+++ src/main/java/com/example/studysprings2025/StudySpring2025Application.java
@@ -0,0 +1,12 @@
+package com.example.studysprings2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpring2025Application {
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpring2025Application.class, args);
+    }
+}
+
